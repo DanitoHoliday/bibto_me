6124f785e15eb78517278d3054cc94714111dd41
Danny Friese <danny.friese@posteo.de>
2020-03-03 11:44:00 +0100

    setup tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+class TagsController < ApplicationController
+  before_action :set_tag, only: [:edit, :update, :destroy]
+
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:code, :category)
+  end
+end
