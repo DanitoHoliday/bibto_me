213c5b6dba3daaf28ee12709337ffab305c9325d
Danny Friese <danny.friese@posteo.de>
2020-03-03 15:56:11 +0100

    controller setup and tags registration

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,18 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: [:edit, :update, :destroy]
+  before_action :set_tag, only: [:update]
 
+  def update_registration
+    if @tag = Tag.find_by(code: params[:code])
+      @tag.registered = true
+      @tag.save
+      redirect_to successful_registration_path
+    else
+      render template: 'pages/registration'
+    end
+  end
+
+  def successful_registration
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :tags, only: [:show, :edit, :update]
+  get 'registration', to: "pages#registration", as: :registration
+  post 'update-registration', to: 'tags#update_registration', as: :update_registration
+  get 'confirmation', to: 'tags#successful_registration', as: :successful_registration
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
