2b69c595cf6f8f480ef8c52cfe3fda41dac61d39
Anouk Escher <anouk.escher@rub.de>
2020-03-03 18:01:43 +0100

    avatar, buttons, search form color-template created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'components',         to: 'pages#components',    as: :components
   resources :tags, only: [:show, :edit, :update]
   resources :finders, only: [:new, :create]
   get '/finders/thank-you', to: 'finders#thank_you', as: :finder_thank_you
