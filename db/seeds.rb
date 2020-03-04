955be7e8990d8602b336bcf8089b23cf8a82cebc
Jan Fuhrmann <zipv3r@gmail.com>
2020-03-04 14:57:25 +0100

    Added new seed file, updated User model with has_one_attached :photo and added the default falue of tags for registered

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,103 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 require "faker"
-
 
 Tag.destroy_all
 User.destroy_all
-
+puts ""
 puts "Creating admin"
-
-x = User.create(first_name: "Armin", last_name: "Admin", email: "admin@gmail.com", password: "123456", phone: "+49 151 1337 1338", shared_phone: true)
+puts ""
+x = User.create(first_name: "Armin", last_name: "Admin", email: "user@bibto.me", password: "123456", phone: "+49 151 1337 1338", shared_phone: true)
 
 puts "Admin-Name | #{x.first_name} #{x.last_name}"
 puts "Admin-Email    | #{x.email}"
 puts "Admin-password | #{x.password}"
+puts ""
+puts "creating already registered tag for Admin"
+puts ""
 
-puts "creating an already registered tag for Admnin"
+tag = Tag.new(code: "ABC123", registered: true,  category: "Sticker", user_id: x.id)
+tag.save
+puts "Tag #1 created #{tag.category} ||  #{tag.code}"
+puts ""
 
-  tag = Tag.new(code: "aa111", registered: true,  category: "Sticker", user_id: x.id)
-  tag.save
-  puts "Tag for admin created"
+tag = Tag.new(code: "XYZ123", registered: true,  category: "Keychain", user_id: x.id)
+tag.save
+puts "Tag #2 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+tag = Tag.new(code: "A1B2C3", registered: true,  category: "Tag", user_id: x.id)
+tag.save
+puts "Tag #3 created #{tag.category} ||  #{tag.code}"
+puts ""
 
 ######################
 
-puts "..."
+puts "#################"
+puts ""
+sleep(1)
 puts "Creating more users"
+puts ""
+puts "#################"
+puts ""
 
 ######################
 
-andy = User.create(first_name: "Andy", last_name: "Baranov", email: "andy@gmail.com", password: "123456", phone: "+49 151 1111 1111", shared_phone: true)
-puts "#{andy.first_name} created"
-
-ryan = User.create(first_name: "Ryan", last_name: "Kretch", email: "ryan@gmail.com", password: "123456", phone: "+49 151 2222 2222", shared_phone: true)
-puts "#{ryan.first_name} created"
-
-leon = User.create(first_name: "Leon", last_name: "Entrup", email: "leon@gmail.com", password: "123456", phone: "+49 151 3333 3333", shared_phone: true)
+leon = User.create(first_name: "Leon", last_name: "Entrup", email: "leon@bibto.me", password: "123456", phone: "+49 151 3333 3333", shared_phone: true)
 puts "#{leon.first_name} created"
+puts "shared_phone = #{leon.shared_phone}"
+puts""
 
-tony = User.create(first_name: "Tony", last_name: "Panacek", email: "tony@gmail.com", password: "123456", phone: "+49 151 4444 4444", shared_phone: true)
+tag = Tag.new(code: "DEV123", registered: true,  category: "Sticker", user_id: leon.id)
+tag.save
+puts "Tag #1 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+tag = Tag.new(code: "GIT123", registered: true,  category: "Keychain", user_id: leon.id)
+tag.save
+puts "Tag #2 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+tag = Tag.new(code: "LEW123", registered: true,  category: "Tag", user_id: leon.id)
+tag.save
+puts "Tag #3 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+puts ""
+puts "#################"
+puts ""
+
+tony = User.create(first_name: "Tony", last_name: "Panacek", email: "tony@bibto.me", password: "123456", phone: "+49 151 4444 4444", shared_phone: false)
 puts "#{tony.first_name} created"
+puts "shared_phone = #{tony.shared_phone}"
+puts ""
 
-kaylin = User.create(first_name: "Kaylin", last_name: "Bittner", email: "kaylin@gmail.com", password: "123456", phone: "+49 151 5555 5555", shared_phone: true)
-puts "#{kaylin.first_name} created"
+tag = Tag.new(code: "USA123", registered: true,  category: "Sticker", user_id: tony.id)
+tag.save
+puts "Tag #1 created #{tag.category} ||  #{tag.code}"
+puts ""
 
+tag = Tag.new(code: "BER123", registered: true,  category: "Keychain", user_id: tony.id)
+tag.save
+puts "Tag #2 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+tag = Tag.new(code: "COD123", registered: true,  category: "Tag", user_id: tony.id)
+tag.save
+puts "Tag #3 created #{tag.category} ||  #{tag.code}"
+puts ""
+
+puts ""
+puts "#################"
+puts ""
 
 ######################
 
-puts 'Creating 50 unregistered tags...'
-40.times do |i|
-  tag = Tag.create!(
-    code: Faker::Alphanumeric.alphanumeric(number: 5),
+puts 'Creating 20 unregistered tags...'
+puts ""
+20.times do |i|
+  i = Tag.create!(
+    code: Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 3, min_numeric: 3),
     registered: false
   )
-  puts "Tag #{tag.code} created"
+  puts "1 | unregistered tag created  -  #{i.code} "
+  puts
 end
-
-puts '###################'
-10.times do |i|
-  tag = Tag.create!(
-    code: Faker::Alphanumeric.alphanumeric(number: 5),
-    registered: true
-  )
-  puts "Tag #{tag.code} created"
-end
-
-
