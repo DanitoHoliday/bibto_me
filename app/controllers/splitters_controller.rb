88329388ad72484b842396975262e38766ca12ca
Danny Friese <danny.friese@posteo.de>
2020-03-09 11:52:03 +0100

    Splitter functionality + some error handling

--- app/controllers/splitters_controller.rb
+++ app/controllers/splitters_controller.rb
@@ -4,10 +4,13 @@ class SplittersController < ApplicationController
     uri = request.original_url
     scanned_code = URI(uri).path.split('/').last
     @tag = Tag.find_by(code: scanned_code)
-    if @tag.registered == true
-      redirect_to register_input_path
+    if @tag.nil?
+      redirect_to root_path
+      flash.alert = "Code not found or not registered yet."
     elsif @tag.registered == false
-      redirect_to finder_input_path
+      redirect_to register_success_path(@tag)
+    elsif @tag.registered == true
+      redirect_to new_tag_finder_path(@tag)
     end
   end
 end
