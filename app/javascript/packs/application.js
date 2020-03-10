21258b1c396f69177a622c10d2be60e5d6cd5008
Anouk Escher <anouk.escher@rub.de>
2020-03-10 20:55:20 +0100

    enabled JS on contact finder page, fiished thank you page - there is still an error - we have to pass the params in the simple form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,15 @@
 import "bootstrap";
 
+const toggler = document.getElementById("checky");
+const reachOutBtn = document.getElementById("reach-out-btn");
+toggler.addEventListener("click", (event) => {
+  if (event.currentTarget.children[0].attributes['aria-expanded'].value === 'false') {
+    reachOutBtn.classList.add('isDisabled')
+  } else {
+    reachOutBtn.classList.remove('isDisabled')
+  }
+  // console.log(reachOutBtn.classList)
+  // reachOutBtn.classList.toggle('isDisabled')
+});
+
+
