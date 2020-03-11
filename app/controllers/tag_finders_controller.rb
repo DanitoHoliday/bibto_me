01df2a40cd1cac54c8cd2f39fbe2b40d9234d507
Jan Fuhrmann <zipv3r@gmail.com>
2020-03-11 14:32:09 +0100

    Twilio done

--- app/controllers/tag_finders_controller.rb
+++ app/controllers/tag_finders_controller.rb
@@ -22,11 +22,17 @@ before_action :authenticate_user!, except: [:new, :create, :thank_you, :finder_i
     @tag = Tag.find_by(id: params[:format])
     @finder = Finder.new(finder_params)
     @finder.tag = @tag
-    @finder.save
-    TwilioController.send_message(@tag.user.phone, @finder.message)
+    if @finder.save
+      full_message = "#{@tag.category} found: #{@finder.phone} / #{@finder.email} | #{@finder.message}"
+      TwilioController.send_message(@tag.user.phone, full_message)
+      redirect_to finder_thank_you_path(@tag)
+    else
+      render :new
+    end
+
     # mail = FinderMailer.with(user: @finder.tag.user, finder: @finder, tag: @finder.tag).notification
     # mail.deliver_now
-    redirect_to finder_thank_you_path(@tag)
+
   end
 
   def finder_input
