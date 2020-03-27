2e0c75e4d361332bd077782a09d425c622aa3cb6
Jan Fuhrmann <zipv3r@gmail.com>
2020-03-27 17:18:48 +0100

    updated Twilio Controller + fixing some style issues in the corresponding views

--- app/controllers/tag_finders_controller.rb
+++ app/controllers/tag_finders_controller.rb
@@ -1,20 +1,20 @@
 class TagFindersController < ApplicationController
 before_action :authenticate_user!, except: [:new, :create, :thank_you, :finder_input]
 
-   def new
-      if params.include?(:code)
-        @tag = Tag.find_by(code: params[:code.downcase])
-        if @tag.nil? || @tag.user.nil?
-          @error = "* Code not found"
-          render :finder_input
-        end
-      elsif params.include?(:format)
-        @tag = Tag.find_by(id: params[:format])
-        if @tag.nil? || @tag.user.nil?
-          @error = "* Code not found"
-          render :finder_input
-        end
+ def new
+    if params.include?(:code)
+      @tag = Tag.find_by(code: params[:code.downcase])
+      if @tag.nil? || @tag.user.nil?
+        @error = "* Code not found"
+        render :finder_input
       end
+    elsif params.include?(:format)
+      @tag = Tag.find_by(id: params[:format])
+      if @tag.nil? || @tag.user.nil?
+        @error = "* Code not found"
+        render :finder_input
+      end
+    end
     @finder = Finder.new
   end
 
@@ -22,17 +22,16 @@ before_action :authenticate_user!, except: [:new, :create, :thank_you, :finder_i
     @tag = Tag.find_by(id: params[:format])
     @finder = Finder.new(finder_params)
     @finder.tag = @tag
+
     if @finder.save
-      full_message = "#{@tag.category} found: #{@finder.phone} / #{@finder.email} | #{@finder.message}"
+      full_message = "Your item was found! Finder’s info: #{@finder.phone} / #{@finder.email} Note: #{@finder.message}"
       TwilioController.send_message(@tag.user.phone, full_message)
       redirect_to finder_thank_you_path(@tag)
     else
       render :new
     end
-
     # mail = FinderMailer.with(user: @finder.tag.user, finder: @finder, tag: @finder.tag).notification
     # mail.deliver_now
-
   end
 
   def finder_input
@@ -40,7 +39,6 @@ before_action :authenticate_user!, except: [:new, :create, :thank_you, :finder_i
 
   def thank_you
     @tag = Tag.find_by(id: params[:format])
-
   end
 
   private
