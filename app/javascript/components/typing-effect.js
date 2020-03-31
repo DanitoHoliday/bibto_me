8b1d43e6bb47e7b6ba5ddd3ea931bcfc39733424
ivan <ivan.tembe@hotmail.com>
2020-04-01 00:43:53 +0200

    First commit - changing branch

--- app/javascript/components/typing-effect.js
+++ app/javascript/components/typing-effect.js
@@ -1,39 +1,34 @@
-// var typeString = ['easy.  ', 'fast.  ', 'secure.  '];
-// var  i = 0;
-// var count = 0
-// var selectedText = '';
-// var text = '';
+var typeString = ['easy.  ', 'fast.  ', 'secure.  '];
+var  i = 0;
+var count = 0
+var selectedText = '';
+var text = '';
 
-// var typingEffect = (function type() {
-//   if (count == typeString.length) {
-//     count = 0;
-//   }
-//   selectedText = typeString[count];
-//   text = selectedText.slice(0, ++i);
-//   document.getElementById('typing').innerHTML = text;
-//   if (text.length === selectedText.length) {
-//     count++;
-//     i = 0;
-//   }
-//   setTimeout(type, 250);
-// }());
+function type() {
+  if (count == typeString.length) {
+    count = 0;
+  }
+  selectedText = typeString[count];
+  text = selectedText.slice(0, ++i);
+  document.getElementById('typing').innerHTML = text;
+  if (text.length === selectedText.length) {
+    count++;
+    i = 0;
+  }
+  setTimeout(type, 250);
+}
 
-// // function sleep(milliseconds) {
-// //   var start = new Date().getTime();
-// //   for (var i = 0; i < 1e7; i++) {
-// //     if ((new Date().getTime() - start) > milliseconds){
-// //       break;
-// //     }
-// //   }
-// // }
+type()
 
-// const sleep = (milliseconds) {
-//   var start = new Date().getTime();
-//   for (var i = 0; i < 1e7; i++) {
-//     if ((new Date().getTime() - start) > milliseconds){
-//       break;
-//     }
-//   }
-// }
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 
-// export {typingEffect, sleep};
+sleep()
+
+export {type, sleep};
