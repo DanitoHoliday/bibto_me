28ddf83c3994b1bf5f731d7801f9bb749d91b246
Danny Friese <danny.friese@posteo.de>
2020-04-08 15:18:19 +0200

    postmark not finished

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_token: Rails.application.credentials.postmark_api_token }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "http://www.bibto.me" }
+  config.action_mailer.postmark_settings   = { api_token: Rails.application.credentials.postmark_api_token }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
