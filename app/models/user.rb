2202508ea9f0b718a098d93bddeb01a18f478240
DannyFriese <dfriese@babbel.com>
2020-10-17 17:12:20 +0200

    fixed username bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :phone, presence: true
-  validates :first_name, presence: true
   validates :email, presence: true
   has_many :tags
   has_many :finders, through: :tags
